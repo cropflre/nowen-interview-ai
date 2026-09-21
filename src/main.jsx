1ef31ae1f6b5dec88d452f4dd643946f49181bb3
Drug <cropflre2333@gmail.com>
2026-09-21 15:59:39 +0800

    feat(v0.9): enforce account gate before loading game and interview components

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import GameShell from './GameShell.jsx';
+import AccountGate from './AccountGate.jsx';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode><GameShell /></React.StrictMode>
+  <React.StrictMode><AccountGate /></React.StrictMode>
 );
